af2fb3ebb507cccd522439efb8df199b4244b49c
Mike Mogenson <michael.mogenson@leaflabs.com>
2016-09-08 10:52:52 -0400

    Added build dir and #pragma once tag to headers

--- alsa_play.h
+++ alsa_play.h
@@ -1,8 +1,5 @@
-#ifndef ALSA_PLAY_H
-#define ALSA_PLAY_H
+#pragma once
 
 int alsa_play(void);
 int alsa_init(char *device_name, char *wav_file, int period);
 void alsa_deinit(void);
-
-#endif /* ALSA_PLAY_H */
--- ftrace.h
+++ ftrace.h
@@ -1,8 +1,5 @@
-#ifndef FTRACE_H
-#define FTRACE_H
+#pragma once
 
 int trace_init(void);
 int trace_start(char *msg);
 int trace_stop(char *msg);
-
-#endif /* FTRACE_H */
